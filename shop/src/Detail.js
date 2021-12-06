0899d7fd7e5cc96737df712f25071026da76586f
syrusriius <syrusriius@gmail.com>
2021-12-06 21:32:05 +0900

    date

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,24 +1,33 @@
 
 import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+let Vox = styled.div `
+    padding : 20px;
+`;
+
+let Topic = styled.h4 `
+    font-size : 25px;
+    color : ${ props => props.color }
+    `;
+
 function Detail(props){
     let history = useHistory();
     let { id } = useParams();
-    let prodNum = props.shoes.find(function(prod) {
-        return prod.id === id;
-    })
-    console.log(prodNum);
-
     return (
         <div className="container">
         <div className="row">
+            <Vox>
+                <Topic color={'Grey'}>상세페이지</Topic>
+            </Vox>
             <div className="col-md-6">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" alt=""/>
             </div>
             <div className="col-md-6 mt-4">
-            <h4 className="pt-5">{props.shoes[prodNum].title}</h4>
-            <p>{props.shoes[prodNum].content}</p>
-            <p>{props.shoes[prodNum].price}</p>
+            <h4 className="pt-5">{props.shoes[id].title}</h4>
+            <p>{props.shoes[id].content}</p>
+            <p>{props.shoes[id].price}</p>
             <button className="btn btn-danger">주문하기</button> 
             <button className="btn btn-danger" onClick={()=>{
                 history.goBack();
